ba02b943fd1c8ea4053f2ac57c6b521728dc247a
youcef101 <youcef.khadem12@gmail.com>
2021-12-20 19:49:40 +0100

    building slider components

--- ecommerce/src/pages/Home.js
+++ ecommerce/src/pages/Home.js
@@ -2,14 +2,18 @@ import React from 'react'
 import styled from 'styled-components'
 import Announcements from '../components/Announcements'
 import Header from '../components/Header'
+import Slider from '../components/Slider'
 function Home() {
     return (
         <Container>
             <Announcements />
             <Header />
+            <Slider />
         </Container>
     )
 }
 
 export default Home
-const Container = styled.div``
+const Container = styled.div`
+
+`
