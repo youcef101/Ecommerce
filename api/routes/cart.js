08d995dc0380de996c76c1b7e995456a1b182ae9
youcef101 <youcef.khadem12@gmail.com>
2021-12-27 15:57:54 +0100

    building cart route all foctionality

--- api/routes/cart.js
+++ api/routes/cart.js
@@ -1,8 +1,80 @@
 import express from 'express'
+import Cart from '../models/Cart.js'
+import { verifyTokens } from '../utils/authTokens.js'
 const router = express.Router()
 
 router.get('/', (req, res) => {
     res.status(200).send('hello from cart route!!!')
 })
+
+//add cart
+router.post('/add', verifyTokens, async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+//edit cart
+router.put("/:cartId", verifyTokens, async (req, res) => {
+    try {
+        if (req.user.id === req.body.userId || req.user.isAdmin === true) {
+            const updatedCart = await Cart.findByIdAndUpdate(
+                req.params.cartId,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
+            res.status(200).json(updatedCart);
+        } else {
+            res.status(400).send('You Are Not Allowed To Update This Cart !!!')
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//delete cart
+router.delete("/:cartId", verifyTokens, async (req, res) => {
+    try {
+        if (req.user.id === req.body.userId || req.user.isAdmin === true) {
+            await Cart.findByIdAndDelete(req.params.cartId);
+            res.status(200).json("Cart has been deleted...");
+        } else {
+            res.status(400).send('You Are Not Allowed To Delete This Cart !!!')
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//get user cart
+router.get("/find/:userId", verifyTokens, async (req, res) => {
+    try {
+        if (req.user.id === req.body.userId || req.user.isAdmin === true) {
+            const cart = await Cart.findOne({ userId: req.params.userId });
+            res.status(200).json(cart);
+        } else {
+            res.status(400).send('You Are Not Allowed To Get User Cart !!!')
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//get all cart
+router.get("/", verifyTokens, async (req, res) => {
+    try {
+        if (req.user.isAdmin === true) {
+            const carts = await Cart.find();
+            res.status(200).json(carts);
+        } else {
+            res.status(400).send('You Are Not Allowed To Get All Cart !!!')
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 export default router
